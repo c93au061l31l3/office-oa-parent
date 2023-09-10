9f828b12ef4c173c014ba9cab5d4049d09c5c4f1
c93au061l31l3 <131161543+c93au061l31l3@users.noreply.github.com>
2023-09-10 17:38:53 +0800

    0910

---
+++ model/src/main/java/com/AlexChang/model/base/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.AlexChang.model.base;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * ClassName:BaseEntity
+ * Description:
+ *
+ * @author Alex
+ * @create 2023-09-10 下午 02:03
+ * @Version:1.0
+ */
+
+@Data
+public class BaseEntity implements Serializable {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    @TableField("create_time")
+    private Date createTime;;
+
+    @TableField("update_time")
+    private Date updateTime;
+
+    @TableLogic
+    @TableField("is_delete")
+    private Integer isDelete;
+
+    /**
+     * @TableField(exist = false) 註解在bean屬性上，表示當前屬性不是數據庫字段，
+     *      但會在項目中使用，這樣在新增...等地方使用bean時，mybatis-plus就會忽略此屬性，不會報錯
+     * */
+    @TableField(exist = false)
+    private Map<String, Objects> param = new HashMap<>();
+
+}
---
+++ model/src/main/java/com/AlexChang/model/process/Process.java
@@ -0,0 +1,12 @@
+package com.AlexChang.model.process;
+
+/**
+ * ClassName:Process
+ * Description:
+ *
+ * @author Alex
+ * @create 2023-09-10 下午 02:20
+ * @Version:1.0
+ */
+public class Process {
+}
